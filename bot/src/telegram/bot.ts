9159c9bc8af7d5f457175be7616e63510d271aa2
preleeex <urazabrodskij671@gmail.com>
2026-06-22 00:14:44 +0300

    Log full chat_join_request payload to diagnose missing query_id

--- bot/src/telegram/bot.ts
+++ bot/src/telegram/bot.ts
@@ -72,6 +72,8 @@ bot.on("chat_join_request", async (ctx) => {
     chatId: chatId.toString(),
     applicant: applicant.id,
     hasQueryId: Boolean(queryId),
+    updateKeys: Object.keys(update),
+    rawUpdate: JSON.stringify(update).slice(0, 1200),
     groupFound: Boolean(group),
     guardEnabled: group?.guardEnabled ?? false,
     removed: Boolean(group?.removedAt),
